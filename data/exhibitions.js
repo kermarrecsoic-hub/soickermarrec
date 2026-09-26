f118685a46ba851602187d61ef4dca775d0461ab
kermarrecsoic-hub <kermarrecsoic@gmail.com>
2026-09-26 15:18:08 +0200

    Create exhibitions.js

---
+++ data/exhibitions.js
@@ -0,0 +1,15 @@
+/* Exhibitions: Neue Projekte als Objekte am Ende des Arrays ergänzen. */
+window.EXHIBITIONS_WORKS = [
+  {
+    title: "", // Hier später den Projekttitel eingeben
+    medium: "",
+    dimensions: "",
+    availability: "",
+    text: "",
+    // Erstes Bild = vorhandene Landingpage-Vorschau; bitte später eigenes Projektfoto einsetzen.
+    images: ["../images/ex26.jpg"],
+  // Für eine Fotoserie weitere Bildpfade in images ergänzen, z. B.:
+  // images: ["../images/architecture/serie/01.jpg", "../images/architecture/serie/02.jpg"],
+  // Alle Bilder erscheinen gleichwertig VOR dem Titel und dem Beschreibungstext.
+  }
+];
