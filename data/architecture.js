a4d405df792ea80d52f0881a389bc1a0aac6cfa6
kermarrecsoic-hub <kermarrecsoic@gmail.com>
2026-09-26 15:19:08 +0200

    Create architecture.js

---
+++ data/architecture.js
@@ -0,0 +1,15 @@
+/* Architecture: Neue Projekte als Objekte am Ende des Arrays ergänzen. */
+window.ARCHITECTURE_WORKS = [
+  {
+    title: "", // Hier später den Projekttitel eingeben
+    medium: "",
+    dimensions: "",
+    availability: "",
+    text: "",
+    // Erstes Bild = vorhandene Landingpage-Vorschau; bitte später eigenes Projektfoto einsetzen.
+    images: ["../images/architecture.jpg"],
+  // Für eine Fotoserie weitere Bildpfade in images ergänzen, z. B.:
+  // images: ["../images/architecture/serie/01.jpg", "../images/architecture/serie/02.jpg"],
+  // Alle Bilder erscheinen gleichwertig VOR dem Titel und dem Beschreibungstext.
+  }
+];
