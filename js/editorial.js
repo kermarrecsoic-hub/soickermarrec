35de496aec1eaf8a9b56038cf9fb15ec8375ef2f
kermarrecsoic-hub <kermarrecsoic@gmail.com>
2026-09-26 15:15:43 +0200

    Create editorial.js

---
+++ js/editorial.js
@@ -0,0 +1,2 @@
+document.addEventListener('contextmenu',e=>{if(e.target.closest('img'))e.preventDefault();});
+document.addEventListener('dragstart',e=>{if(e.target.closest('img'))e.preventDefault();});
