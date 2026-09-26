8c43f3eec0c195d4f4215163ea8855f14cf3b176
kermarrecsoic-hub <kermarrecsoic@gmail.com>
2026-09-26 15:17:25 +0200

    Create graphic.js

---
+++ data/graphic.js
@@ -0,0 +1,13 @@
+/* Graphic: Neue Projekte als Objekte am Ende des Arrays ergänzen. */
+window.GRAPHIC_WORKS = [
+  {
+    title: "", // Hier später den Projekttitel eingeben
+    medium: "",
+    dimensions: "",
+    availability: "",
+    text: "",
+    // Erstes Bild = vorhandene Landingpage-Vorschau; bitte später eigenes Projektfoto einsetzen.
+    images: ["../images/graphic.jpg"],
+  // Optional: images: ["../images/graphic/projekt/01.jpg", "../images/graphic/projekt/02.jpg"]
+  }
+];
